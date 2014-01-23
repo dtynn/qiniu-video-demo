a7b70366872e39acd3694eb7297d5bd402b2b0ab
dtynn <dtynn@163.com>
2014-01-23 15:15:54 +0800

    上传1

---
+++ code/1/upload_basic.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.path.insert(0, '../')
+
+from qiniu import conf as qConf, rs as qRs, io as qIo
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
+BUCKET = ''
+QINIU_DOMAIN = ''
+
+qConf.ACCESS_KEY = ACCESS_KEY
+qConf.SECRET_KEY = SECRET_KEY
+
+putPolicy = qRs.PutPolicy(scope=BUCKET)
+uptoken = putPolicy.token()
+
+filePath = '../files/test.jpg'
+
+key = 'upload_basic.jpg'
+ret, err = qIo.put_file(uptoken, key, filePath)
+print ret
+print err
+print 'http://%s/%s' % (QINIU_DOMAIN, key)
